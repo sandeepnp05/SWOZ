42fa0a556b77df69ded1de3f5c190f62827babe1
sandeepnp05 <npsandeep05@gmail.com>
2023-08-01 12:03:09 +0530

    added product in admin side

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,33 +1,54 @@
-const express = require('express')
-const admin_route = express()
-const adminController = require('../controllers/adminController')
-const nocache = require('nocache')
-const auth = require('../middleware/adminAuth')
+const express = require("express");
+const admin_route = express();
+const adminController = require("../controllers/adminController");
+const nocache = require("nocache");
+const auth = require("../middleware/adminAuth");
+const Products = require("../models/productModel");
+const upload = require("../middleware/imageUpload");
 
+admin_route.set("view engine", "ejs");
+admin_route.set("views", "./views/admin");
 
-admin_route.set('view engine' , 'ejs')
-admin_route.set('views' , './views/admin')
+admin_route.use(nocache());
 
-admin_route.use(nocache())
+admin_route.get("/", auth.isLogout, adminController.adminLoad);
+admin_route.get("/dashboard", auth.isLogin, adminController.dashboardLoad);
+admin_route.post("/", adminController.adminVerifyLogin);
+admin_route.get("/logout", adminController.logout);
+admin_route.get("/userList", auth.isLogin, adminController.userList);
+admin_route.get("/blockUser", adminController.blockUser);
 
+admin_route.get("/categories", auth.isLogin, adminController.categories);
+admin_route.post("/categories", auth.isLogin, adminController.addCategories);
+admin_route.get("/editCategory", auth.isLogin, adminController.editCategories);
+admin_route.post(
+  "/editCategory",
+  auth.isLogin,
+  adminController.updatedCategory
+);
+admin_route.patch("/listCategory", auth.isLogin, adminController.listCategory);
 
-admin_route.get('/' ,auth.isLogout,adminController.adminLoad )
-admin_route.get('/dashboard',auth.isLogin, adminController.dashboardLoad);
-admin_route.post('/', adminController.adminVerifyLogin)
-admin_route.get('/logout', adminController.logout)
-admin_route.get('/userList', auth.isLogin,adminController.userList)
-admin_route.get('/blockUser',adminController.blockUser)
+admin_route.get(
+  "/productAddPage",
+  auth.isLogin,
+  adminController.productAddPage
+);
+admin_route.get(
+  "/productEditPage",
+  auth.isLogin,
+  adminController.productEditPage
+);
+admin_route.get(
+  "/productListPage",
+  auth.isLogin,
+  adminController.productListPage
+);
 
-admin_route.get('/categories', auth.isLogin, adminController.categories);
-admin_route.post('/categories', auth.isLogin, adminController.addCategories);
-admin_route.get('/editCategory',auth.isLogin, adminController.editCategories)
-admin_route.post('/editCategory',auth.isLogin, adminController.updatedCategory)
-admin_route.patch('/listCategory', auth.isLogin, adminController.listCategory);
-admin_route.get('/productAddPage',auth.isLogin,adminController.productAddPage)
-admin_route.get('/productEditPage',auth.isLogin,adminController.productEditPage)
-admin_route.get('/productListPage',auth.isLogin,adminController.productListPage)
+admin_route.post(
+  "/productAddPage",
+  auth.isLogin,
+  upload.array("product_img", 4),
+  adminController.productAdd
+);
 
-
-
-
-module.exports = admin_route
+module.exports = admin_route;
