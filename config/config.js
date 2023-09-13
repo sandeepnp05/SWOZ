ee1d72cd983b5061446fc465bbb586c883cfab07
sandeepnp05 <npsandeep05@gmail.com>
2023-09-13 19:33:01 +0530

    last commit3

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const sessionSecret = 'mysessionsecret'
 
 const mongooConnect =()=>{
     
- mongoose.connect('mongodb://127.0.0.1:27017/swoz_online')
+ mongoose.connect('mongodb+srv://Sandeep:G9PiGosfFRZXocoP@swoz.3vjj8te.mongodb.net/swoz_online')
 .then(()=> console.log('connected'))
 .catch((err)=> console.log(err.message)) 
 }
